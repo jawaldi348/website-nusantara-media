f6af01435d1ef76809926370c8f3a8a5096710fc
Jawaldi <46097155+jawaldi348@users.noreply.github.com>
2022-10-25 19:59:19 +0700

    pisahkan section navbar

--- app/Views/layouts/index.php
+++ app/Views/layouts/index.php
@@ -28,7 +28,7 @@
     <link rel="stylesheet" href="<?= assets() ?>lib/flickity/flickity.css" media="screen">
 
     <!-- Customized Bootstrap Stylesheet -->
-    <link href="<?= assets() ?>css/style.css?v1.3-beta1" rel="stylesheet">
+    <link rel="stylesheet" href="<?= assets() ?>css/style.css">
 </head>
 
 <body>
@@ -47,70 +47,8 @@
     </div>
     <!-- Topbar End -->
 
-
     <!-- Navbar Start -->
-    <div class="container-fluid p-0 mb-3">
-        <nav class="navbar navbar-expand-lg bg-navbar navbar-light py-2 py-lg-0 px-lg-5">
-            <a href="" class="navbar-brand d-block d-lg-none">
-                <h1 class="m-0 display-5 text-uppercase"><span class="text-primary">News</span>Room</h1>
-            </a>
-            <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
-                <span class="navbar-toggler-icon">
-                    <i class="fas fa-bars" style="color:#fff; font-size:28px;"></i>
-                </span>
-            </button>
-            <div class="collapse navbar-collapse justify-content-between px-0 px-lg-3" id="navbarCollapse">
-                <ul class="navbar-nav bg-navbar mr-auto py-0">
-                    <li class="d-inline d-lg-none">
-                        <div class="nav-wrapper-logo">
-                            <a href="">
-                                <h1 class="m-0 display-5 text-uppercase"><span class="text-primary">News</span>Room</h1>
-                            </a>
-                            <div style="padding: 0 0 0 30px">
-                                <button data-toggle="collapse" data-target="#navbarCollapse" class="close float-right">
-                                    <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 32 32">
-                                        <path fill="currentColor" d="M24 9.4L22.6 8L16 14.6L9.4 8L8 9.4l6.6 6.6L8 22.6L9.4 24l6.6-6.6l6.6 6.6l1.4-1.4l-6.6-6.6L24 9.4z"></path>
-                                    </svg>
-                                </button>
-                            </div>
-                        </div>
-                    </li>
-                    <li class="nav-item">
-                        <a href="index.html" class="nav-link active">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="category.html" class="nav-link">Categories</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="single.html" class="nav-link">Single News</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="#">Action</a>
-                            <a class="dropdown-item" href="#">Another action</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="#">Something else here</a>
-                        </div>
-                    </li>
-                    <li class="nav-item">
-                        <a href="contact.html" class="nav-link">Contact</a>
-                    </li>
-                </ul>
-                <div class="d-none d-lg-block">
-                    <form method="post" class="nm-searchform" action="#" autocomplete="off">
-                        <input type="text" name="search" id="search" placeholder="Cari">
-                        <button type="submit">
-                            <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24">
-                                <path fill="currentColor" d="M10 18a7.952 7.952 0 0 0 4.897-1.688l4.396 4.396l1.414-1.414l-4.396-4.396A7.952 7.952 0 0 0 18 10c0-4.411-3.589-8-8-8s-8 3.589-8 8s3.589 8 8 8zm0-14c3.309 0 6 2.691 6 6s-2.691 6-6 6s-6-2.691-6-6s2.691-6 6-6z"></path>
-                                <path fill="currentColor" d="M11.412 8.586c.379.38.588.882.588 1.414h2a3.977 3.977 0 0 0-1.174-2.828c-1.514-1.512-4.139-1.512-5.652 0l1.412 1.416c.76-.758 2.07-.756 2.826-.002z"></path>
-                            </svg>
-                        </button>
-                    </form>
-                </div>
-            </div>
-        </nav>
-    </div>
+    <?= $this->include('layouts/navbar') ?>
     <!-- Navbar End -->
 
     <!-- Top News Slider Start -->
