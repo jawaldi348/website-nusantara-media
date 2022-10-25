2916b641afe5aa074fbcc3ae08a863bb92781106
Jawaldi <46097155+jawaldi348@users.noreply.github.com>
2022-10-25 17:47:40 +0700

    model get all settingan umum website

---
+++ app/Models/Settings/Mgeneral.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models\Settings;
+
+use CodeIgniter\Model;
+
+class Mgeneral extends Model
+{
+    public function fetch_all($params)
+    {
+        $this->builder = $this->db->table('settings');
+        if ($params != 'all') {
+            $this->builder->where(['name' => $params]);
+        }
+        $res = $this->builder->get();
+        // $result = null;
+        if ($params == 'all') {
+            foreach ($res->getResult() as $re) {
+                if ($re->name == 'logo' or $re->name == 'favicon') {
+                    $result[$re->name] = getenv('urlassets') . 'main/' . $re->value;
+                } else {
+                    $result[$re->name] = $re->value;
+                }
+            }
+            // $result = (object)$result;
+        } else {
+            foreach ($res->getResult() as $re) {
+                $result[] = $re->value;
+            }
+        }
+        return $result;
+    }
+}
