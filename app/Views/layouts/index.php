a8328f17dc1925d8fcfa7a7496872ff940061b46
Jawaldi <46097155+jawaldi348@users.noreply.github.com>
2022-10-25 13:52:53 +0700

    tambahkan bagian berita tranding

--- app/Views/layouts/index.php
+++ app/Views/layouts/index.php
@@ -25,7 +25,7 @@
     <link rel="stylesheet" href="<?= assets() ?>lib/flickity/flickity.css" media="screen">
 
     <!-- Customized Bootstrap Stylesheet -->
-    <link href="<?= assets() ?>css/style.css" rel="stylesheet">
+    <link href="<?= assets() ?>css/style.css?v1.3-beta1" rel="stylesheet">
 </head>
 
 <body>
@@ -109,6 +109,49 @@
         </nav>
     </div>
     <!-- Navbar End -->
+
+    <!-- Top News Slider Start -->
+    <div class="container">
+        <div class="row">
+            <div class="col-2 col-sm-1 col-md-3 col-lg-2 py-1 pe-md-0 mb-md-1">
+                <div class="d-inline-block d-md-block bg-primary text-white text-center breaking-caret py-1 px-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="1rem" height="1rem" fill="currentColor" class="bi bi-lightning-fill" viewBox="0 0 16 16">
+                        <path d="M11.251.068a.5.5 0 0 1 .227.58L9.677 6.5H13a.5.5 0 0 1 .364.843l-8 8.5a.5.5 0 0 1-.842-.49L6.323 9.5H3a.5.5 0 0 1-.364-.843l8-8.5a.5.5 0 0 1 .615-.09z" />
+                    </svg>
+                    <span class="d-none d-md-inline-block">Tranding</span>
+                </div>
+            </div>
+            <div class="col-10 col-sm-11 col-md-9 col-lg-10 ps-1 ps-md-2">
+                <!-- <div class="d-flex justify-content-between"> -->
+                <div class="breaking-box position-relative py-2">
+                    <div class="box-carousel" data-flickity='{ "cellAlign": "left", "wrapAround": true, "adaptiveHeight": true, "prevNextButtons": true , "autoPlay": 5000, "pageDots": false, "imagesLoaded": true }'>
+                        <!--list post-->
+                        <div class="col-12 aribudin">
+                            <a class="h6 fw-normal" href="#">Google Employees Protest Secret Work on Censored Search Engine for China</a>
+                        </div>
+                        <!--list post-->
+                        <div class="col-12 aribudin">
+                            <a class="h6 fw-normal" href="#">Investors Betting Against Tesla Made $1 Billion on Friday</a>
+                        </div>
+                        <!--list post-->
+                        <div class="col-12 aribudin">
+                            <a class="h6 fw-normal" href="#">Carmelo Anthony Officially Signs with the Houston Rockets</a>
+                        </div>
+                        <!--list post-->
+                        <div class="col-12 aribudin">
+                            <a class="h6 fw-normal" href="#">Lemon make your skin fresh and glowing</a>
+                        </div>
+                        <!--list post-->
+                        <div class="col-12 aribudin">
+                            <a class="h6 fw-normal" href="#">5 Takeaways From Elon Musk’s Interview With The Times About Tesla</a>
+                        </div>
+                    </div>
+                </div>
+                <!-- </div> -->
+            </div>
+        </div>
+    </div>
+    <!-- Top News Slider End -->
 
     <!-- News With Sidebar Start -->
     <div class="container py-3">
