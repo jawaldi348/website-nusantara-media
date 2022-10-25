f7c7e0f28bf93570c3acd4ee6010e5a6494ba924
Jawaldi <46097155+jawaldi348@users.noreply.github.com>
2022-10-25 18:14:29 +0700

    tambahkan tag pendukung lainnya

--- app/Views/layouts/home.php
+++ app/Views/layouts/home.php
@@ -3,4 +3,31 @@
 <meta name="description" content="<?= $config['sitedescription'] ?>">
 <meta name="keywords" content="<?= $config['sitedescription'] ?>">
 <meta name="news_keywords" content="<?= $config['sitedescription'] ?>">
+
+<meta name="language" content="id">
+<meta name="geo.country" content="id">
+<meta http-equiv="content-language" content="In-Id">
+<meta name="geo.placename" content="Indonesia">
+<meta name="theme-color" content="#5da5e0">
+<!-- s: fb meta -->
+<meta property="fb:app_id" content="">
+<meta property="fb:pages" content="">
+<meta property="og:title" content="<?= $title ?>">
+<meta property="og:description" content="<?= $config['sitedescription'] ?>">
+<meta property="og:type" content="article">
+<meta property="og:url" content="<?= $config['site_domain'] ?>">
+<meta property="og:image" content="<?= $config['logo'] ?>">
+<meta property="og:image:type" content="image/jpeg">
+<meta property="og:image:width" content="457">
+<meta property="og:image:height" content="100">
+<meta property="og:site_name" content="<?= $config['domainname'] ?>">
+<!-- e: fb meta -->
+<!-- S:tweeter card -->
+<meta name="twitter:card" content="summary_large_image">
+<meta name="twitter:site" content="<?= $config['domainname'] ?>">
+<meta name="twitter:title" content="<?= $title ?>">
+<meta name="twitter:description" content="<?= $config['sitedescription'] ?>">
+<meta name="twitter:image" content="<?= $config['logo'] ?>">
+<meta name="twitter:image:src" content="<?= $config['logo'] ?>">
+<!-- E:tweeter card -->
 <?= $this->endSection(); ?>
