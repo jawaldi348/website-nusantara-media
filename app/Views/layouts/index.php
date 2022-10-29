ee024779a6a45c90280908479e2416f0e68ac7a7
Jawaldi <46097155+jawaldi348@users.noreply.github.com>
2022-10-29 09:42:16 +0700

    tambah section top-bar

--- app/Views/layouts/index.php
+++ app/Views/layouts/index.php
@@ -33,18 +33,7 @@
 
 <body>
     <!-- Topbar Start -->
-    <div class="container-fluid">
-        <div class="row align-items-center py-2 px-lg-5">
-            <div class="col-lg-4">
-                <a href="<?= site_url() ?>" class="navbar-brand d-none d-lg-block">
-                    <img src="<?= $config['logo'] ?>" class="img-fluid" alt="<?= $config['sitename'] ?>">
-                </a>
-            </div>
-            <div class="col-lg-8 text-center text-lg-right">
-                <img class="img-fluid" src="<?= assets() ?>img/ads-700x70.jpg" alt="<?= $config['sitename'] ?>">
-            </div>
-        </div>
-    </div>
+    <?= $this->include('layouts/top-bar') ?>
     <!-- Topbar End -->
 
     <!-- Navbar Start -->
