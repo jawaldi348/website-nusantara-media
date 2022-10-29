75399c6ddd531bf0653d4575a07122c60fc6b3b2
Jawaldi <46097155+jawaldi348@users.noreply.github.com>
2022-10-29 08:49:37 +0700

    tambah section sidebar

---
+++ app/Views/layouts/sidebar.php
@@ -0,0 +1,103 @@
+<div class="col-lg-4 pt-3 pt-lg-0">
+    <!-- Ads Start -->
+    <div class="pb-3">
+        <a href="#"><img class="img-fluid w-100" src="<?= assets() ?>img/news-500x280-4.jpg" alt=""></a>
+    </div>
+    <!-- Ads End -->
+
+    <!-- Social Follow Start -->
+    <div class="pb-3">
+        <h3 class="py-2 mb-3 section-title">
+            <span>Ikuti Kami</span>
+        </h3>
+        <div class="d-flex mb-3">
+            <a href="<?= $config['facebook'] ?>" target="_blank" class="d-block w-50 py-2 px-3 text-white text-decoration-none mr-2" style="background: #39569E;">
+                <small class="fab fa-facebook-f mr-2"></small><small>Facebook</small>
+            </a>
+            <a href="<?= $config['twitter'] ?>" target="_blank" class="d-block w-50 py-2 px-3 text-white text-decoration-none ml-2" style="background: #52AAF4;">
+                <small class="fab fa-twitter mr-2"></small><small>Twitter</small>
+            </a>
+        </div>
+        <div class="d-flex mb-3">
+            <a href="<?= $config['youtube'] ?>" target="_blank" class="d-block w-50 py-2 px-3 text-white text-decoration-none mr-2" style="background: #DC472E;">
+                <small class="fab fa-youtube mr-2"></small><small>Youtube</small>
+            </a>
+            <a href="<?= $config['instagram'] ?>" target="_blank" class="d-block w-50 py-2 px-3 text-white text-decoration-none ml-2" style="background: #C8359D;">
+                <small class="fab fa-instagram mr-2"></small><small>Instagram</small>
+            </a>
+        </div>
+    </div>
+    <!-- Social Follow End -->
+
+    <!-- Ads Start -->
+    <div class="pb-3">
+        <a href=""><img class="img-fluid" src="<?= assets() ?>img/news-500x280-4.jpg" alt=""></a>
+    </div>
+    <!-- Ads End -->
+
+    <!-- Popular News Start -->
+    <div class="pb-3">
+        <h3 class="py-2 section-title">
+            <span>Terpopuler</span>
+        </h3>
+        <div class="sidebar-article article-list-container">
+            <div class="article-list-row">
+                <div class="article-list-thumb">
+                    <a href="#" class="article-list-thumb-link flex_ori" title="#">
+                        <img src="<?= assets() ?>img/news-100x100-3.jpg" class="img-fluid w-100" alt="#" style="object-fit: cover;">
+                    </a>
+                </div>
+                <div class="position-relative px-0">
+                    <div class="article-list-info content_center">
+                        <span>
+                            <a href="#" class="article-list-title" title="#">
+                                <h2>Lorem ipsum dolor sit amet consec adipis elit</h2>
+                            </a>
+                            <a href="#" class="article-list-cate content_center" title="#">
+                                <h3>Technology</h3>
+                            </a>
+                            <div class="article-list-date content_center">
+                                <span>January 01, 2045</span>
+                            </div>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <button class="btn btn-nm btn-block btn-more content_center">
+            <span>
+                <div>Selengkapnya</div>
+                <i class="fas fa-angle-double-right"></i>
+            </span>
+        </button>
+    </div>
+    <!-- Popular News End -->
+
+    <!-- Ads Start -->
+    <div class="pb-3">
+        <a href=""><img class="img-fluid" src="<?= assets() ?>img/news-500x280-4.jpg" alt=""></a>
+    </div>
+    <!-- Ads End -->
+
+    <!-- Tags Start -->
+    <div class="pb-3">
+        <div class="bg-light py-2 px-4 mb-3">
+            <h3 class="m-0">Tags</h3>
+        </div>
+        <div class="d-flex flex-wrap m-n1">
+            <a href="" class="btn btn-sm btn-outline-secondary m-1">Politics</a>
+            <a href="" class="btn btn-sm btn-outline-secondary m-1">Business</a>
+            <a href="" class="btn btn-sm btn-outline-secondary m-1">Corporate</a>
+            <a href="" class="btn btn-sm btn-outline-secondary m-1">Sports</a>
+            <a href="" class="btn btn-sm btn-outline-secondary m-1">Health</a>
+            <a href="" class="btn btn-sm btn-outline-secondary m-1">Education</a>
+            <a href="" class="btn btn-sm btn-outline-secondary m-1">Science</a>
+            <a href="" class="btn btn-sm btn-outline-secondary m-1">Technology</a>
+            <a href="" class="btn btn-sm btn-outline-secondary m-1">Foods</a>
+            <a href="" class="btn btn-sm btn-outline-secondary m-1">Entertainment</a>
+            <a href="" class="btn btn-sm btn-outline-secondary m-1">Travel</a>
+            <a href="" class="btn btn-sm btn-outline-secondary m-1">Lifestyle</a>
+        </div>
+    </div>
+    <!-- Tags End -->
+</div>
