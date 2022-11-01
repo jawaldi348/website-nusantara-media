9650b6e44e29fd0d440cb8a7c0962d0ea42e908d
Jawaldi <46097155+jawaldi348@users.noreply.github.com>
2022-11-01 21:09:24 +0700

    meta google site verification

--- app/Views/layouts/index.php
+++ app/Views/layouts/index.php
@@ -6,6 +6,7 @@
     <title><?= $title ?></title>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="google-site-verification" content="cMrxbAYatSBJiW9MedBFTOAj-YRc0jqOoTRgutx3jKU" />
 
     <!-- s: section meta -->
     <?= $this->renderSection('tagmeta') ?>
