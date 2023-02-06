f7985c6bb9bd488d60f4290bc0f8626a432b3e0f
Jawaldi <46097155+jawaldi348@users.noreply.github.com>
2023-02-06 11:05:54 +0700

    tambah function lainnya pada helper component

--- app/Helpers/component_helper.php
+++ app/Helpers/component_helper.php
@@ -6,3 +6,32 @@ if (!function_exists("assets")) {
         return $assets;
     }
 }
+
+if (!function_exists('getIPAddress')) {
+    function getIPAddress()
+    {
+        $request = \Config\Services::request();
+        return $request->getIPAddress();
+    }
+}
+
+if (!function_exists('strTrim')) {
+    function strTrim($str)
+    {
+        if (!empty($str)) {
+            return trim($str);
+        }
+    }
+}
+
+if (!function_exists('cleanNumber')) {
+    function cleanNumber($num)
+    {
+        $num = strTrim($num);
+        $num = esc($num);
+        if (empty($num)) {
+            return 0;
+        }
+        return intval($num);
+    }
+}
